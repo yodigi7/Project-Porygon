889f9024d975ccc32069e554364e27ab587fc97e
BrockmeyerTyler <tjb101@yahoo.com>
2018-02-17 18:00:13 -0600

    Added signup capability, session tracking, and login capability. HTML is still required.

--- visualization/website.py
+++ visualization/website.py
@@ -1,30 +1,74 @@
-from flask import Flask, request, render_template, session, abort
+from flask import Flask, request, render_template, session, abort, url_for, redirect, flash
 import os
 
 app = Flask(__name__)
+app.secret_key = "This isn't very secret"
+
+users = {}
+
+
+# Decorator to redirect to login page if not logged in.
+def require_login(func):
+	def closure(*args, **kwargs):
+		if 'username' not in session:
+			return redirect(url_for('login'))
+		return func(*args, **kwargs)
+	return closure
+
+
+def user_exists(username):
+	return username in users
+
+
+def valid_login(username, password):
+	return username in users and users[username] == password
+
 
 @app.route('/')
+@require_login
 def home():
-	#if not session.get('logged_in'):
-	#	return render_template('login.html')
-	#else:
-		return render_template('home.html')
+	return render_template('home.html')
 
-@app.route('/leaderboard_page/')
+
+@app.route('/leaderboard/')
+@require_login
 def leaderboard():
 	return render_template('leaderboard.html')
 
-@app.route('/battle_page/')
+
+@app.route('/battle/')
+@require_login
 def battle():
 	return render_template('battle.html')
 
-@app.route('/account_page/')
+
+@app.route('/account/')
+@require_login
 def account():
 	return render_template('account.html')
 
-@app.route('/login_page/')
+
+@app.route('/login/')
 def login():
-	return render_template('login.html')
+	if request.method == 'GET':
+		return render_template('login.html')
+	if request.method == 'POST':
+		if not valid_login(request.form['username'], request.form['password']):
+			flash("Invalid username or password.")
+			return render_template('login.html')
+
+
+@app.route('/signup/')
+def signup():
+	if request.method == 'GET':
+		return render_template('signup.html')
+	if request.method == 'POST':
+		if user_exists(request.form['username']):
+			flash("That username is already in use.")
+			return render_template('signup.html')
+		users[request.form['username']] = request.form['password']
+		session['username'] = request.form['username']
+		return redirect(url_for('home'))
 
 if __name__ == '__main__':
 	app.run(debug = True)
